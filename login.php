a9d1b938fa863df021128ae70be313d5fb2325e6
Jafet Suárez <jafet.suarezxbox@hotmail.com>
2020-12-08 23:46:29 -0600

    Removiendo estilos 2

--- login.php
+++ login.php
@@ -9,9 +9,9 @@
 		
 		<div style="text-align: center;">
 		<form method="post" action="$PORTAL_ACTION$" id="loginForm" style="display: block">
-			<div><input name="auth_user" type="text" placeholder="Username" class='form-control'></div>
-			<div><input name="auth_pass" type="password" placeholder="Password" class='form-control'></div>
-			<div><input name="auth_voucher" type="text" placeholder="Voucher" class='form-control'></div>
+			<div><input name="auth_user" type="text" placeholder="Username" ></div>
+			<div><input name="auth_pass" type="password" placeholder="Password" ></div>
+			<div><input name="auth_voucher" type="text" placeholder="Voucher" ></div>
 			<input name="redirurl" type="hidden" value="$PORTAL_REDIRURL$">
 			<input name="zone" type="hidden" value="$PORTAL_ZONE$">
 			<div><input name="accept" type="submit" value="Continue"></div>
@@ -19,13 +19,13 @@
 
 		<form method="post" id="signUpForm" style="display: none">
 			<div>
-				<input name="userName" type="text" placeholder="Full Name" class='form-control'>
+				<input name="userName" type="text" placeholder="Full Name" >
 			</div>
 			<div>
-				<input name="email" type="text" placeholder="Email" class='form-control'>
+				<input name="email" type="text" placeholder="Email" >
 			</div>
 			<div>
-				<input name="password" type="password" placeholder="Password" class='form-control'>
+				<input name="password" type="password" placeholder="Password" >
 			</div>
 			<div>
 				<input name="signup" type="submit" value="Sign up">
